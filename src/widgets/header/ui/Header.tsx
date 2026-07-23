74c28a7b5e3094c98233a549402a6c525c99b954
Dasha <shpakovad@gmail.com>
2026-07-23 20:25:22 +0300

    Added language switcher

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,12 +3,26 @@ import { Button } from "@/src/shared/ui/button";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border-b border-transparent p-4 dark:border-[var(--chart-3)]">
-      <div className="flex items-center">
-        <Sparkles color="oklch(70.7% 0.165 254.624)" />
-        <span className="pl-3 text-sm font-semibold text-slate-100">JobMatch AI</span>
+    <header className="flex flex-col items-center justify-between border-b border-transparent pb-4 pt-4 dark:border-[var(--chart-3)]">
+      <div className="mb-4 flex w-full justify-end">
+        <div
+          className="flex h-9 items-center overflow-hidden p-1 text-xs font-semibold text-slate-300 shadow-sm"
+          aria-label="Language switcher"
+        >
+          <span className="px-1 py-1 text-slate-50 shadow-sm hover:cursor-pointer">EN</span>
+          <span>/</span>
+          <span className="px-1 py-1 text-slate-400 hover:cursor-pointer">RU</span>
+        </div>
       </div>
-      <Button>Button</Button>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex items-end">
+          <Sparkles color="oklch(70.7% 0.165 254.624)" />
+          <span className="pl-3 text-sm font-semibold text-slate-100">JobMatch AI</span>
+        </div>
+        <div className="ml-auto flex items-center gap-3">
+          <Button>Войти</Button>
+        </div>
+      </div>
     </header>
   );
 };
