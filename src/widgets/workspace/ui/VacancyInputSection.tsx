59121169c218ce57bd44a3cbad593e9bcff03a5e
Dasha <shpakovad@gmail.com>
2026-07-28 20:21:28 +0300

    Added VacancyInputSection UI

--- src/widgets/workspace/ui/VacancyInputSection.tsx
+++ src/widgets/workspace/ui/VacancyInputSection.tsx
@@ -1,12 +1,15 @@
 import { Textarea } from "@/src/shared/ui/textarea";
+import { useTranslations } from "next-intl";
 
 export const VacancyInputSection = () => {
+  const t = useTranslations("WorkSpacePage.VacancyInputSection");
   return (
-    <div>
-      <p className="pb-2 font-mono text-sm text-slate-400">
-        Step 2/2 · Paste a job URL or description
-      </p>
-      <Textarea placeholder="Job URL or description" />
+    <div className="mb-14">
+      <p className="pb-2 text-center font-mono text-sm text-slate-400">{t("step")}</p>
+      <Textarea
+        className="min-h-[300px] min-w-[500px]"
+        placeholder={`${t("textareaPlaceholder")}`}
+      />
     </div>
   );
 };
--- src/widgets/workspace/ui/WorkSpacePage.tsx
+++ src/widgets/workspace/ui/WorkSpacePage.tsx
@@ -1,15 +1,17 @@
+import { useTranslations } from "next-intl";
 import { UploadResumeSection } from "./UploadResumeSection";
 import { VacancyInputSection } from "./VacancyInputSection";
-import { useTranslations } from "next-intl";
+import { Button } from "@/src/shared/ui/button";
 
 export const WorkSpacePage = () => {
   const t = useTranslations("WorkSpacePage");
 
   return (
-    <div className="flex w-full flex-col items-center justify-center pt-14">
+    <div className="flex w-full flex-col items-center justify-center pb-14 pt-14">
       <p className="pb-10 text-primary">{t("title")}</p>
       <UploadResumeSection />
       <VacancyInputSection />
+      <Button>Analyze</Button>
     </div>
   );
 };
