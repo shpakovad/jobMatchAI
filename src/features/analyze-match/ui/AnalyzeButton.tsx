63a69b7edac57a80242084df99cdad96a2a7933d
Dasha <shpakovad@gmail.com>
2026-07-29 17:47:18 +0300

    Added state for Analysis

--- src/features/analyze-match/ui/AnalyzeButton.tsx
+++ src/features/analyze-match/ui/AnalyzeButton.tsx
@@ -2,9 +2,11 @@
 
 import { Button } from "@/src/shared/ui/button";
 import { useAnalysisStore } from "@/src/entities/analysis";
+import { useTranslations } from "next-intl";
 
 export const AnalyzeButton = () => {
   const isReady = useAnalysisStore((state) => state.getIsReady());
+  const t = useTranslations("WorkSpacePage");
 
-  return <Button disabled={!isReady}>Analyze</Button>;
+  return <Button disabled={!isReady}>{t("analyzeButtonLabel")}</Button>;
 };
