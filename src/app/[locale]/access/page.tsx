8d4aea65a1557c8216c42d8de79fc822f04f4f4d
Dasha <shpakovad@gmail.com>
2026-08-13 14:58:16 +0300

    Code refactoring

--- src/app/[locale]/access/page.tsx
+++ src/app/[locale]/access/page.tsx
@@ -1,7 +1,8 @@
 import { withServerAccess } from "@/src/shared/hoc";
 import { DemoAccessedPage, DemoAccessPage } from "@/src/widgets/demo-access";
 
-const DemoAccess = withServerAccess(async ({ sessionId }: { sessionId: string }) => {
+const DemoAccess = withServerAccess(async ({ sessionId }: { sessionId?: string }) => {
+
   if (sessionId) {
     return <DemoAccessedPage />;
   }
