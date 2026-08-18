b74f1023da5a27b6328d32234b80b40efde3377e
Dasha <shpakovad@gmail.com>
2026-08-18 20:41:17 +0300

    Changed global error page

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useTranslations } from "next-intl";
+import "./[locale]/globals.css";
+
 import { startTransition, useEffect } from "react";
 
 import { ErrorIcon } from "@/src/shared/icons/ErrorIcon";
@@ -12,7 +13,6 @@ interface GlobalErrorProps {
 }
 
 export default function GlobalError({ error, reset }: GlobalErrorProps) {
-  const t = useTranslations("GlobalErrorPage");
 
   useEffect(() => {
     console.error(error);
@@ -35,12 +35,13 @@ export default function GlobalError({ error, reset }: GlobalErrorProps) {
           </div>
 
           <div className="space-y-2">
-            <h1 className="text-xl font-bold tracking-tight text-slate-100">{t("title")}</h1>
-            <p className="text-sm leading-relaxed text-slate-400">{t("description")}</p>
+            <h1 className="text-xl font-bold tracking-tight text-slate-100">Ooops...</h1>
+            <p className="text-sm leading-relaxed text-slate-400">Возникла неожиданная ошибка в работе приложения. Мы уже выявили проблему и работаем над её исправлением.</p>
+            <p className="text-sm leading-relaxed text-slate-400">There was an unexpected glitch in the app interface. We've already identified the issue and are working on a fix.</p>
           </div>
 
           <Button variant="secondary" onClick={handleAppReset}>
-            {t("restartAppLabel")}
+            Перезапустить приложение / Restart App
           </Button>
         </div>
       </body>
