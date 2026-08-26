8fcf0d22530b1ce334d95be3c29b0b5ed3322f03
Dasha <shpakovad@gmail.com>
2026-08-26 14:34:07 +0300

    Fixed prisma settings

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig = {
   reactCompiler: true,
 
   outputFileTracingIncludes: {
-    "/**/*": ["./src/generated/prisma/**/*"]
+    "/**/*": ["./src/generated/prisma/**/*.node"],
   },
 
   async headers() {
